477eb2a1a10391ca5a9dcf467edeb72180f72ff5
charo-esliceu <charoh@esliceu.net>
2026-02-03 10:01:16 +0100

    tres enraya

--- sript.js
+++ sript.js
@@ -1,5 +1,10 @@
-let btnRef = document.querySelectorAll(".button");
+let btn = document.querySelectorAll(".button");
 let restartBtn = document.getElementById("restart");
+let message = document.getElementById("message");
+
+
+//definir las casillas ganadoras 
+
 let winningPattern = [
     [0, 1, 2],
     [0, 3, 6],
@@ -12,6 +17,38 @@ let winningPattern = [
 ];
 
 //player X play first
-
-let xTurn = true;
+let Turn = true;
 let count = 0;
+
+//
+
+const winchecker = () => {
+    for (let i of winningPattern){
+        let [element1, element2, element3] = [
+            btnRef[i[0]].innerText,
+            btnRef[i[1]].innerText,
+            btnRef[i[2]].innerText,
+        ];
+    }
+     (element1 != "" && (element2 != "") & (element3 !=  ""))
+}
+
+btn.forEach((element) =>{
+    element.addEventListener("click", () =>{
+
+//boquear una casilla ocupadas     
+
+    if (element.innerText !=="")return;   
+  
+    if (Turn) {
+        Turn = false;
+        element.innerText = "X";
+        element.disabled = true;
+    }
+    else{
+        Turn = true;
+        element.innerText = "O";
+        element.disabled = true;
+    }
+    });
+});
